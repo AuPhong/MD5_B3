b6731572d5c569ea7f2097de83938b41d878a07b
au phong <stthu98@gmail.com>
2022-07-01 14:06:12 +0700

    hacker-new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import { ShowPetInfoComponent } from './show-pet-info/show-pet-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShowPetInfoComponent
+
   ],
     imports: [
         BrowserModule,
